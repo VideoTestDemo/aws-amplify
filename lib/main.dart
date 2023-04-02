d90025e748dd99dc033dd3d9cd4751a2a655500f
adam <402299499@qq.com>
2023-04-02 18:00:20 +0900

    change

--- lib/main.dart
+++ lib/main.dart
@@ -7,60 +7,27 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'aws-amplify',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'aws-amplify'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              color: Colors.red,
-              width: MediaQuery.of(context).size.width,
-              height: 200,
-            ),
-            Container(
-              color: Colors.blue,
-              width: MediaQuery.of(context).size.width,
-              height: 200,
-            ),
-            Container(
-              color: Colors.yellow,
-              width: MediaQuery.of(context).size.width,
-              height: 200,
-            )
-          ],
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Container(
+          color: Colors.red,
+          width: 100,
+          height: 200,
+        ),
+        Container(
+          color: Colors.blue,
+          width: 200,
+          height: 200,
+        ),
+        Container(
+          color: Colors.yellow,
+          width: 300,
+          height: 200,
+        )
+      ],
     );
   }
 }
