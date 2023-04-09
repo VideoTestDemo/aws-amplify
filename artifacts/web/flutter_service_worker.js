cb01679a1c13d0aee4558c1c4f7bedcdd6aeb5df
adam <402299499@qq.com>
2023-04-09 22:50:19 +0900

    modified artifacts

--- artifacts/web/flutter_service_worker.js
+++ artifacts/web/flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "f5bf0d8d8c92a8b9de276a062938e796",
-"index.html": "d04f7c24114c278e8b271417d0d55b9a",
-"/": "d04f7c24114c278e8b271417d0d55b9a",
-"main.dart.js": "e7a6ab8b0d27ff326f01fc06d8dab6c2",
+"index.html": "479dafb39493def6806c94b661be7828",
+"/": "479dafb39493def6806c94b661be7828",
+"main.dart.js": "6e1684ab1d19bea4dda0dd9f6843bc86",
 "flutter.js": "1cfe996e845b3a8a33f57607e8b09ee4",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
